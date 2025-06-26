a1c30774421b17030e2eef3f9c776cae91784bac
unknown <manoranjanjenamitsy17@gmail.com>
2025-06-26 21:59:13 +0530

    Basic Setup for the backend side

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+// Old Version 
+// const express = require('express');
+// const app =express();
+// module.exports=app; 
+
+import express from 'express';
+const app =express();
+export default app;
---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config({ path: "backend/config/config.env" });
+// console.log(app)
+const port = process.env.PORT || 3000;
+
+const getAllProducts = (req, res) => {
+  res.status(200).json({
+    message: "All Products",
+  });
+};
+
+
+const getSingleProduct = (req, res) => {
+  res.status(200).json({
+    message: "Single Product",
+  });
+};
+
+
+// 1 Way 
+// app.get("/api/v1/products", getAllProducts);
+
+// app.get("/api/v1/product", getSingleProduct);
+
+// -----------------------------------------------------------------
+
+// 2nd Way
+
+// app.route("/api/v1/products").get(getAllProducts)
+// app.route("/api/v1/product").get(getSingleProduct);
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+});
