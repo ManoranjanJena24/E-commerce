e118da494de6e071ea155bc8b5d7471d6623c947
unknown <manoranjanjenamitsy17@gmail.com>
2025-07-03 02:28:12 +0530

    Done with some more errors

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ import { connectMongoDatabase } from "./config/db.js";
 dotenv.config({ path: "backend/config/config.env" });
 // console.log(app)
 connectMongoDatabase();
+
+// Handle uncaught exception errors
+process.on('uncaughtException',(err)=>{
+  console.log(`Error : ${err.message}`);
+  console.log(`Server is shutting down due to uncaught exception errors`)
+  process.exit(1)
+})
 const port = process.env.PORT || 3000;
 
 // const getAllProducts = (req, res) => {
@@ -35,6 +42,17 @@ const port = process.env.PORT || 3000;
 
 
 
-app.listen(port, () => {
+const server=  app.listen(port, () => {
   console.log(`Server is running on PORT ${port}`);
+
 });
+
+// console.log(myName)
+
+process.on('unhandledRejection',(err)=>{
+  console.log(`Error: ${err.message}`);
+  console.log(`Server is shutting down,due to unhandled promise rejection`);
+  server.close(()=>{
+    process.exit(1)
+  })
+})
